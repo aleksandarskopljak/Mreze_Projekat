5d31c413a97c57ce46b17f9f18cb5153ec8571f3
aleksandarskopljak <aleksandarskopljak9@gmail.com>
2026-02-08 23:19:39 +0100

    Kreiranje filijale i klijenta

--- Filijala/Filijala.cs
+++ Filijala/Filijala.cs
@@ -1,20 +1,387 @@
-﻿using Common;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Runtime.CompilerServices;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using Common;
 
 namespace Filijala
 {
-    public class Filijala
+    class Program
     {
-        
-       
-        
+        private static int _filijalaId;
+        private static Socket _udpSocket;
+        private static Socket _tcpListenerSocket;
+        private static EndPoint _serverEndPoint;
+        private static Enkriptor _enkriptor;
+        private static bool _aktivan = true;
+        private static List<Socket> _povezaniKlijenti = new List<Socket>();
+        private static object _lock = new object();
+        private static string _enkripcioniKljuc = null;
+
+        private static int _serverPort = 5000;
+        private static string _serverIP = "127.0.0.1";
+        private static int _tcpPort = 6000;
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("=== FILIJALA BANKE ===\n");
+
+            if (args.Length >= 1)
+                int.TryParse(args[0], out _filijalaId);
+            else
+            {
+                Console.Write("Unesite ID filijale: ");
+                int.TryParse(Console.ReadLine(), out _filijalaId);
+            }
+
+            if (args.Length >= 2)
+                int.TryParse(args[1], out _tcpPort);
+            else
+                _tcpPort = 6000 + _filijalaId;
+
+            Console.WriteLine($"Filijala ID: {_filijalaId}");
+            Console.WriteLine($"TCP port za klijente: {_tcpPort}");
+
+            try
+            {
+
+                _udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                _udpSocket.Bind(new IPEndPoint(IPAddress.Any, 0));
+                _serverEndPoint = new IPEndPoint(IPAddress.Parse(_serverIP), _serverPort);
+
+
+                RegistrujKodServera();
+
+
+                _tcpListenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                _tcpListenerSocket.Bind(new IPEndPoint(IPAddress.Any, _tcpPort));
+                _tcpListenerSocket.Listen(10);
+                _tcpListenerSocket.Blocking = false;
+                Console.WriteLine($"TCP listener soket pokrenut na portu {_tcpPort}");
+
+
+                Thread klijentThread = new Thread(PrihvatajKlijente);
+                klijentThread.IsBackground = true;
+                klijentThread.Start();
+
+                Console.WriteLine("\nFilijala je aktivna. Pritisnite 'Q' za izlaz.\n");
+
+                while (_aktivan)
+                {
+                    if (Console.KeyAvailable)
+                    {
+                        var key = Console.ReadKey(true);
+                        if (key.Key == ConsoleKey.Q)
+                        {
+                            _aktivan = false;
+                            break;
+                        }
+                    }
+
+                    Thread.Sleep(100);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Greska: {ex.Message}");
+            }
+            finally
+            {
+                Zatvori();
+            }
+        }
+
+        static void RegistrujKodServera()
+        {
+            Console.WriteLine("Registracija kod centralnog servera...");
+
+            Poruka registracija = new Poruka(TipPoruke.Registracija)
+            {
+                FilijalaId = _filijalaId,
+                Sadrzaj = $"Filijala_{_filijalaId}:{_tcpPort}"
+            };
+
+            PosaljiServeru(registracija);
+
+
+            try
+            {
+                _udpSocket.ReceiveTimeout = 5000;
+                byte[] buffer = new byte[65535];
+                EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+                int primljeno = _udpSocket.ReceiveFrom(buffer, ref remoteEP);
+
+                string odgovorStr = Encoding.UTF8.GetString(buffer, 0, primljeno);
+                Poruka odgovor = Serijalizator.DeserijalizujIzStringa<Poruka>(odgovorStr);
+
+                if (odgovor.Tip == TipPoruke.DistribucijaKljuca)
+                {
+                    _enkripcioniKljuc = odgovor.Sadrzaj;
+                    _enkriptor = new Enkriptor(_enkripcioniKljuc);
+                    Console.WriteLine("Enkripcioni kljuc primljen od servera.");
+                }
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Server nije dostupan. Nastavlja se bez enkripcije.");
+            }
+            finally
+            {
+                _udpSocket.ReceiveTimeout = 0;
+            }
+        }
+
+        static void PosaljiServeru(Poruka poruka)
+        {
+            string porukaStr = Serijalizator.SerijalizujUString(poruka);
+
+            if (_enkriptor != null)
+            {
+                porukaStr = _enkriptor.Sifruj(porukaStr);
+            }
+
+            byte[] podaci = Encoding.UTF8.GetBytes(porukaStr);
+            _udpSocket.SendTo(podaci, _serverEndPoint);
+        }
+
+        static Poruka CekajOdgovorServera()
+        {
+            int pokusaji = 0;
+            while (pokusaji < 50)
+            {
+                try
+                {
+
+                    if (_udpSocket.Poll(100000, SelectMode.SelectRead))
+                    {
+                        byte[] buffer = new byte[65535];
+                        EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+                        int primljeno = _udpSocket.ReceiveFrom(buffer, ref remoteEP);
+
+                        string porukaStr = Encoding.UTF8.GetString(buffer, 0, primljeno);
+
+                        Poruka poruka = null;
+
+
+                        try
+                        {
+                            poruka = Serijalizator.DeserijalizujIzStringa<Poruka>(porukaStr);
+                        }
+                        catch
+                        {
+                            if (_enkriptor != null)
+                            {
+                                try
+                                {
+                                    string desifrovano = _enkriptor.Desifruj(porukaStr);
+                                    poruka = Serijalizator.DeserijalizujIzStringa<Poruka>(desifrovano);
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine($"Greska pri desifrovanju: {ex.Message}");
+                                    continue;
+                                }
+                            }
+                        }
+
+                        if (poruka != null)
+                        {
+                            if (poruka.Tip == TipPoruke.DistribucijaKljuca)
+                            {
+                                _enkripcioniKljuc = poruka.Sadrzaj;
+                                _enkriptor = new Enkriptor(_enkripcioniKljuc);
+                                Console.WriteLine("Novi enkripcioni kljuc primljen.");
+                            }
+                            return poruka;
+                        }
+                    }
+                }
+                catch (SocketException ex)
+                {
+                    if (ex.SocketErrorCode != SocketError.WouldBlock)
+                        Console.WriteLine($"Socket greska: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Greska u CekajOdgovorServera: {ex.Message}");
+                }
+
+                pokusaji++;
+            }
+
+            return new Poruka(TipPoruke.Greska, "Server ne odgovara");
+        }
+
+        static void PrihvatajKlijente()
+        {
+            Console.WriteLine("Cekanje na klijente...");
+
+            while (_aktivan)
+            {
+                try
+                {
+
+                    if (_tcpListenerSocket.Poll(50000, SelectMode.SelectRead))
+                    {
+                        Socket klijentSocket = _tcpListenerSocket.Accept();
+                        Console.WriteLine($"Novi klijent povezan: {klijentSocket.RemoteEndPoint}");
+
+                        lock (_lock)
+                        {
+                            _povezaniKlijenti.Add(klijentSocket);
+                        }
+
+                        Thread klijentHandler = new Thread(() => ObradiKlijenta(klijentSocket));
+                        klijentHandler.IsBackground = true;
+                        klijentHandler.Start();
+                    }
+
+
+                    List<Socket> zaUklanjanje = new List<Socket>();
+                    lock (_lock)
+                    {
+                        foreach (var klijent in _povezaniKlijenti)
+                        {
+                            if (!klijent.Connected)
+                                zaUklanjanje.Add(klijent);
+                        }
+                        foreach (var klijent in zaUklanjanje)
+                        {
+                            _povezaniKlijenti.Remove(klijent);
+                            klijent.Close();
+                        }
+                    }
+                }
+                catch (SocketException ex)
+                {
+                    if (_aktivan && ex.SocketErrorCode != SocketError.WouldBlock)
+                        Console.WriteLine($"Greska pri prihvatanju klijenta: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    if (_aktivan)
+                        Console.WriteLine($"Greska: {ex.Message}");
+                }
+            }
+        }
+
+        static void ObradiKlijenta(Socket klijentSocket)
+        {
+            try
+            {
+
+                if (_enkripcioniKljuc != null)
+                {
+                    Poruka kljucPoruka = new Poruka(TipPoruke.DistribucijaKljuca, _enkripcioniKljuc);
+                    PosaljiKlijentu(klijentSocket, kljucPoruka, false);
+                }
+
+                byte[] buffer = new byte[4096];
+
+                while (_aktivan && klijentSocket.Connected)
+                {
+
+                    if (klijentSocket.Poll(10000, SelectMode.SelectRead))
+                    {
+                        int bytesRead = klijentSocket.Receive(buffer);
+                        if (bytesRead == 0)
+                            break;
+
+                        string porukaStr = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                        if (_enkriptor != null)
+                        {
+                            porukaStr = _enkriptor.Desifruj(porukaStr);
+                        }
+
+                        Poruka poruka = Serijalizator.DeserijalizujIzStringa<Poruka>(porukaStr);
+                        Console.WriteLine($"Zahtev od klijenta: {poruka.Tip}");
+
+                        Poruka odgovor = ObradiKlijentZahtev(poruka);
+                        PosaljiKlijentu(klijentSocket, odgovor, true);
+                    }
+                }
+            }
+            catch (SocketException ex)
+            {
+                if (ex.SocketErrorCode != SocketError.ConnectionReset)
+                    Console.WriteLine($"Socket greska sa klijentom: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Greska u komunikaciji sa klijentom: {ex.Message}");
+            }
+            finally
+            {
+                lock (_lock)
+                {
+                    _povezaniKlijenti.Remove(klijentSocket);
+                }
+                klijentSocket.Close();
+                Console.WriteLine("Klijent disconnected.");
+            }
+        }
+
+        static void PosaljiKlijentu(Socket klijentSocket, Poruka poruka, bool sifruj)
+        {
+            string porukaStr = Serijalizator.SerijalizujUString(poruka);
+
+            if (sifruj && _enkriptor != null)
+            {
+                porukaStr = _enkriptor.Sifruj(porukaStr);
+            }
+
+            byte[] podaci = Encoding.UTF8.GetBytes(porukaStr);
+            klijentSocket.Send(podaci);
+        }
+
+        static Poruka ObradiKlijentZahtev(Poruka zahtev)
+        {
+            switch (zahtev.Tip)
+            {
+                case TipPoruke.Prijava:
+                    return ProslediServeru(zahtev);
+                case TipPoruke.PregledStanja:
+                    Poruka stanje = new Poruka(TipPoruke.ProveriStanje)
+                    {
+                        KorisnikId = zahtev.KorisnikId,
+                        FilijalaId = _filijalaId
+                    };
+                    return ProslediServeru(stanje);
+                case TipPoruke.Uplata:
+                case TipPoruke.Isplata:
+                case TipPoruke.Transfer:
+                case TipPoruke.Registracija:
+                    return ProslediServeru(zahtev);
+                default:
+                    return new Poruka(TipPoruke.Greska, "Nepoznat zahtev");
+            }
+        }
+
+        static Poruka ProslediServeru(Poruka zahtev)
+        {
+            zahtev.FilijalaId = _filijalaId;
+            PosaljiServeru(zahtev);
+            return CekajOdgovorServera();
+        }
+
+        static void Zatvori()
+        {
+            Console.WriteLine("\nZatvaranje filijale...");
+            _aktivan = false;
+
+            lock (_lock)
+            {
+                foreach (var klijent in _povezaniKlijenti)
+                    klijent.Close();
+                _povezaniKlijenti.Clear();
+            }
+
+            _tcpListenerSocket?.Close();
+            _udpSocket?.Close();
+        }
     }
 }
--- Klijent/Program.cs
+++ Klijent/Program.cs
@@ -1,15 +1,328 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using Common;
 
-namespace Klijent
+namespace Client
 {
-    internal class Program
+    class Program
     {
+        private static Socket _tcpSocket;
+        private static Enkriptor _enkriptor;
+        private static string _enkripcioniKljuc;
+        private static int? _prijavljeniKorisnikId = null;
+        private static bool _aktivan = true;
+
+        private static string _filijalaIP = "127.0.0.1";
+        private static int _filijalaPort = 6001;
+
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("=== KLIJENT BANKE ===\n");
+
+            if (args.Length >= 1)
+            {
+                int.TryParse(args[0], out _filijalaPort);
+            }
+            else
+            {
+                Console.Write("Unesite port filijale (default 6001): ");
+                string portInput = Console.ReadLine();
+                if (!string.IsNullOrEmpty(portInput))
+                    int.TryParse(portInput, out _filijalaPort);
+                if (_filijalaPort == 0) _filijalaPort = 6001;
+            }
+
+            try
+            {
+
+                Console.WriteLine($"Povezivanje na filijalu {_filijalaIP}:{_filijalaPort}...");
+                _tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                _tcpSocket.Connect(new IPEndPoint(IPAddress.Parse(_filijalaIP), _filijalaPort));
+                Console.WriteLine("Povezano na filijalu!");
+
+
+                PrimiKljuc();
+
+                while (_aktivan)
+                {
+                    PrikaziMeni();
+                    ObradiIzbor();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Greska: {ex.Message}");
+            }
+            finally
+            {
+                _tcpSocket?.Close();
+                Console.WriteLine("Klijent zatvoren.");
+            }
+        }
+
+        static void PrimiKljuc()
+        {
+            try
+            {
+
+                _tcpSocket.ReceiveTimeout = 5000;
+                byte[] buffer = new byte[4096];
+                int bytesRead = _tcpSocket.Receive(buffer);
+
+                if (bytesRead > 0)
+                {
+                    string porukaStr = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Poruka poruka = Serijalizator.DeserijalizujIzStringa<Poruka>(porukaStr);
+
+                    if (poruka.Tip == TipPoruke.DistribucijaKljuca)
+                    {
+                        _enkripcioniKljuc = poruka.Sadrzaj;
+                        _enkriptor = new Enkriptor(_enkripcioniKljuc);
+                        Console.WriteLine("Enkripcioni kljuc primljen.\n");
+                    }
+                }
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Kljuc nije primljen. Nastavlja se bez enkripcije.\n");
+            }
+            finally
+            {
+                _tcpSocket.ReceiveTimeout = 0;
+            }
+        }
+
+        static void PrikaziMeni()
+        {
+            Console.WriteLine("\n--- MENI ---");
+
+            if (_prijavljeniKorisnikId == null)
+            {
+                Console.WriteLine("1. Prijava");
+                Console.WriteLine("2. Registracija novog korisnika");
+            }
+            else
+            {
+                Console.WriteLine($"Prijavljeni ste (ID: {_prijavljeniKorisnikId})");
+                Console.WriteLine("1. Pregled stanja");
+                Console.WriteLine("2. Uplata");
+                Console.WriteLine("3. Isplata");
+                Console.WriteLine("4. Transfer");
+                Console.WriteLine("5. Odjava");
+            }
+            Console.WriteLine("0. Izlaz");
+            Console.Write("\nIzbor: ");
+        }
+
+        static void ObradiIzbor()
+        {
+            string izbor = Console.ReadLine();
+
+            if (_prijavljeniKorisnikId == null)
+            {
+                switch (izbor)
+                {
+                    case "1": Prijava(); break;
+                    case "2": Registracija(); break;
+                    case "0": _aktivan = false; break;
+                    default: Console.WriteLine("Nepoznat izbor."); break;
+                }
+            }
+            else
+            {
+                switch (izbor)
+                {
+                    case "1": PregledStanja(); break;
+                    case "2": Uplata(); break;
+                    case "3": Isplata(); break;
+                    case "4": Transfer(); break;
+                    case "5": Odjava(); break;
+                    case "0": _aktivan = false; break;
+                    default: Console.WriteLine("Nepoznat izbor."); break;
+                }
+            }
+        }
+
+        static void Prijava()
+        {
+            Console.Write("Unesite ime: ");
+            string ime = Console.ReadLine();
+            Console.Write("Unesite lozinku: ");
+            string lozinka = Console.ReadLine();
+
+            Poruka zahtev = new Poruka(TipPoruke.Prijava, $"{ime}:{lozinka}");
+            Poruka odgovor = PosaljiIprimiOdgovor(zahtev);
+
+            if (odgovor.Tip == TipPoruke.Uspeh)
+            {
+                _prijavljeniKorisnikId = odgovor.KorisnikId ?? int.Parse(odgovor.Sadrzaj);
+                Console.WriteLine($"Prijava uspesna! ID: {_prijavljeniKorisnikId}");
+            }
+            else
+            {
+                Console.WriteLine($"Greska: {odgovor.Sadrzaj}");
+            }
+        }
+
+        static void Registracija()
+        {
+            Console.Write("Unesite ime: ");
+            string ime = Console.ReadLine();
+            Console.Write("Unesite prezime: ");
+            string prezime = Console.ReadLine();
+            Console.Write("Unesite lozinku: ");
+            string lozinka = Console.ReadLine();
+            Console.Write("Unesite pocetno stanje: ");
+            decimal.TryParse(Console.ReadLine(), out decimal pocetnoStanje);
+
+            Poruka zahtev = new Poruka(TipPoruke.Registracija, $"{ime}:{prezime}:{lozinka}:{pocetnoStanje}");
+            Poruka odgovor = PosaljiIprimiOdgovor(zahtev);
+
+            Console.WriteLine(odgovor.Tip == TipPoruke.Uspeh
+                ? $"Registracija uspesna! {odgovor.Sadrzaj}"
+                : $"Greska: {odgovor.Sadrzaj}");
+        }
+
+        static void PregledStanja()
+        {
+            Poruka zahtev = new Poruka(TipPoruke.PregledStanja)
+            {
+                KorisnikId = _prijavljeniKorisnikId
+            };
+
+            Poruka odgovor = PosaljiIprimiOdgovor(zahtev);
+
+            if (odgovor.Tip == TipPoruke.StanjeOdgovor || odgovor.Tip == TipPoruke.Uspeh)
+                Console.WriteLine($"\nVase stanje: {odgovor.Sadrzaj} RSD");
+            else
+                Console.WriteLine($"Greska: {odgovor.Sadrzaj}");
+        }
+
+        static void Uplata()
+        {
+            Console.Write("Unesite iznos za uplatu: ");
+            if (!decimal.TryParse(Console.ReadLine(), out decimal iznos))
+            {
+                Console.WriteLine("Neispravan iznos.");
+                return;
+            }
+
+            Poruka zahtev = new Poruka(TipPoruke.Uplata)
+            {
+                KorisnikId = _prijavljeniKorisnikId,
+                Iznos = iznos
+            };
+
+            Poruka odgovor = PosaljiIprimiOdgovor(zahtev);
+            Console.WriteLine(odgovor.Tip == TipPoruke.Uspeh
+                ? $"Uspeh: {odgovor.Sadrzaj}"
+                : $"Greska: {odgovor.Sadrzaj}");
+        }
+
+        static void Isplata()
+        {
+            Console.Write("Unesite iznos za isplatu: ");
+            if (!decimal.TryParse(Console.ReadLine(), out decimal iznos))
+            {
+                Console.WriteLine("Neispravan iznos.");
+                return;
+            }
+
+            Poruka zahtev = new Poruka(TipPoruke.Isplata)
+            {
+                KorisnikId = _prijavljeniKorisnikId,
+                Iznos = iznos
+            };
+
+            Poruka odgovor = PosaljiIprimiOdgovor(zahtev);
+            Console.WriteLine(odgovor.Tip == TipPoruke.Uspeh
+                ? $"Uspeh: {odgovor.Sadrzaj}"
+                : $"Greska: {odgovor.Sadrzaj}");
+        }
+
+        static void Transfer()
+        {
+            Console.Write("Unesite ID primaoca: ");
+            if (!int.TryParse(Console.ReadLine(), out int primalacId))
+            {
+                Console.WriteLine("Neispravan ID.");
+                return;
+            }
+
+            Console.Write("Unesite iznos za transfer: ");
+            if (!decimal.TryParse(Console.ReadLine(), out decimal iznos))
+            {
+                Console.WriteLine("Neispravan iznos.");
+                return;
+            }
+
+            Poruka zahtev = new Poruka(TipPoruke.Transfer)
+            {
+                KorisnikId = _prijavljeniKorisnikId,
+                PrimalacId = primalacId,
+                Iznos = iznos
+            };
+
+            Poruka odgovor = PosaljiIprimiOdgovor(zahtev);
+            Console.WriteLine(odgovor.Tip == TipPoruke.Uspeh
+                ? $"Uspeh: {odgovor.Sadrzaj}"
+                : $"Greska: {odgovor.Sadrzaj}");
+        }
+
+        static void Odjava()
+        {
+            _prijavljeniKorisnikId = null;
+            Console.WriteLine("Odjavljeni ste.");
+        }
+
+        static Poruka PosaljiIprimiOdgovor(Poruka zahtev)
+        {
+            try
+            {
+                // Serijalizuj i sifruj
+                string porukaStr = Serijalizator.SerijalizujUString(zahtev);
+
+                if (_enkriptor != null)
+                {
+                    porukaStr = _enkriptor.Sifruj(porukaStr);
+                }
+
+                byte[] podaci = Encoding.UTF8.GetBytes(porukaStr);
+                _tcpSocket.Send(podaci);
+
+
+                byte[] buffer = new byte[4096];
+                int pokusaji = 0;
+                while (pokusaji < 100)
+                {
+                    if (_tcpSocket.Poll(100000, SelectMode.SelectRead))
+                    {
+                        int bytesRead = _tcpSocket.Receive(buffer);
+                        if (bytesRead > 0)
+                        {
+                            string odgovorStr = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                            if (_enkriptor != null)
+                            {
+                                odgovorStr = _enkriptor.Desifruj(odgovorStr);
+                            }
+
+                            return Serijalizator.DeserijalizujIzStringa<Poruka>(odgovorStr);
+                        }
+                    }
+                    pokusaji++;
+                }
+
+                return new Poruka(TipPoruke.Greska, "Timeout - filijala ne odgovara");
+            }
+            catch (Exception ex)
+            {
+                return new Poruka(TipPoruke.Greska, ex.Message);
+            }
         }
     }
 }
